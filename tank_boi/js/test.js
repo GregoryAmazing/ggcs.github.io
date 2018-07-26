47ea94b34f2f0f9b5fe6506c413a64575f531f1b
Gregory Romanovsky <gregoryromanovsky255@gmail.com>
2018-07-26 13:34:38 +0500

    bye bye

--- tank_boi/js/test.js
+++
@@ -1,8 +0,0 @@
-if (met.x < boi.x + boi.w &&
-    met.x + met.w > boi.x &&
-    met.y < boi.y + boi.h &&
-    met.h + met.y > boi.y) {
-     // collision detected!
- }
- 
-//SIMPLE AABB 2D COLLISION
