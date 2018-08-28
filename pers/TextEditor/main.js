ade69327d5a6720d2b03a22dd85daaf58969c982
Gregory Romanovsky <gregoryromanovsky255@gmail.com>
2018-08-29 03:03:43 +0500

    TextEditor added!
    trollol

---
+++ pers/TextEditor/main.js
@@ -0,0 +1,84 @@
+'use strict'
+
+var screenW, screenH;
+
+screenW = window.innerWidth;
+screenH = window.innerHeight;
+
+var curText = 'hay guys leafy here!';
+
+var stage = new Konva.Stage({
+  container: 'container',
+  width: screenW,
+  height: screenH
+});
+
+var layer = new Konva.Layer();
+stage.add(layer);
+
+var shape = new Konva.Text({
+  x: 400,
+  y: 100,
+  fill: 'red',
+  fontSize: 69,
+  fontFamily: 'Calibri',
+  text: curText,
+  draggable: true
+});
+
+shape.cache();
+shape.filters([Konva.Filters.HSL]);
+layer.add(shape);
+
+function setPathText(node,text)
+{
+  node.clearCache()
+  node.text(text);
+  layer.draw();
+  node.cache()
+  layer.batchDraw();
+}
+
+var angularSpeed = 200;
+
+var anim = new Konva.Animation(function(frame) {
+  var angleDiff = frame.timeDiff * angularSpeed / 1000;
+  shape.rotate(angleDiff);
+}, layer);
+
+
+document.addEventListener('keyup', e=>{
+  if(e.keyCode === 32)
+  {
+    console.log('Started');
+    anim.start();
+  }
+  else if(e.keyCode === 88 && anim.isRunning() === true)
+  {
+    console.log('Stoped');
+    anim.stop();
+  }
+  
+  if(e.keyCode === 73)
+  {
+    console.log('heeey');
+  }
+
+  if (e.keyCode === 13)
+  {
+    
+  }
+    
+})
+
+
+var sliders = ['hue', 'saturation', 'luminance'];
+sliders.forEach(function (attr) {
+  var slider = document.getElementById(attr);
+  function update() {
+    shape[attr](parseFloat(slider.value));
+    layer.batchDraw();
+  }
+  slider.oninput = update;
+  update();
+});
