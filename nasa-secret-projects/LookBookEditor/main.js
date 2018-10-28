7c7feb6b28d324ccdde330ed8130980f636c112e
Gregory Romanovsky <gregoryromanovsky255@gmail.com>
2018-10-28 11:54:37 +0500

    3 пункт пока что глючит на мобильных устройствах так что я его отключил

--- nasa-secret-projects/LookBookEditor/main.js
+++ nasa-secret-projects/LookBookEditor/main.js
@@ -228,26 +228,26 @@ border.evented = false;
 border.controls = false;
 border.selectable = false;
 
-canvas.on('mouse:down', function(opt) {
-    var evt = opt.e;
-    if (canvas.getActiveObjects().length == 0) {
-      this.isDragging = true;
-      this.selection = false;
-      this.lastPosX = evt.clientX;
-      this.lastPosY = evt.clientY;
-    }
-  });
-  canvas.on('mouse:move', function(opt) {
-    if (this.isDragging) {
-      var e = opt.e;
-      this.viewportTransform[4] += e.clientX - this.lastPosX;
-      this.viewportTransform[5] += e.clientY - this.lastPosY;
-      this.requestRenderAll();
-      this.lastPosX = e.clientX;
-      this.lastPosY = e.clientY;
-    }
-  });
-  canvas.on('mouse:up', function(opt) {
-    this.isDragging = false;
-    this.selection = true;
-  });
+// canvas.on('mouse:down', function (opt) {
+//     var evt = opt.e;
+//     if (canvas.getActiveObjects().length == 0) {
+//         this.isDragging = true;
+//         this.selection = false;
+//         this.lastPosX = evt.clientX;
+//         this.lastPosY = evt.clientY;
+//     }
+// });
+// canvas.on('mouse:move', function (opt) {
+//     if (this.isDragging) {
+//         var e = opt.e;
+//         this.viewportTransform[4] += e.clientX - this.lastPosX;
+//         this.viewportTransform[5] += e.clientY - this.lastPosY;
+//         this.requestRenderAll();
+//         this.lastPosX = e.clientX;
+//         this.lastPosY = e.clientY;
+//     }
+// });
+// canvas.on('mouse:up', function (opt) {
+//     this.isDragging = false;
+//     this.selection = true;
+// });
