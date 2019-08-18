59616ec9d00fefbe10bed7fd0fefc81e0d8d854b
Gregory Romanovsky <gregoryromanovsky255@gmail.com>
2019-08-18 19:38:33 +0500

    test 1

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-$.ajax({
-	type:"GET", 
-	url: "https://api.github.com/repos/romanovsky-g/romanovsky-g.github.io/contents/", 
-	success: function(data) {
-		  $("body").append(JSON.stringify(data));
-	    }, 
-	error: function(jqXHR, textStatus, errorThrown) {
-		  alert(jqXHR.status);
-	    },
-     dataType: "jsonp"
-  });​​​​​​​​​​​​​​​
+console.log('Hello wrld!');
