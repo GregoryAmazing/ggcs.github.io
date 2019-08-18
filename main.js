a1fd028295e190d7277ca123fac52521dc037584
Gregory Romanovsky <gregoryromanovsky255@gmail.com>
2019-08-18 19:40:29 +0500

    test 2

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Hello wrld!');
+$.ajax({
+    type: 'GET',
+    url:
+        'https://api.github.com/repos/romanovsky-g/romanovsky-g.github.io/contents/',
+    success: function(data) {
+        $('body').append(JSON.stringify(data));
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        alert(jqXHR.status);
+    },
+    dataType: 'jsonp'
+});
