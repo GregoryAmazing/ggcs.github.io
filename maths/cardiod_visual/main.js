117daacd9db7607fd4cd485a439f6127d722c90a
Gregory Romanovsky <gregoryromanovsky225@gmail.com>
2021-07-14 03:14:30 +0500

    cardiod dpi fix attempt

--- maths/cardiod_visual/main.js
+++ maths/cardiod_visual/main.js
@@ -8,6 +8,20 @@ canvas.width = window.innerWidth;
 let cx = canvas.getContext('2d');
 let scale = 1;
 
+let dpi = window.devicePixelRatio;
+
+function fix_dpi() {
+    //get CSS height
+    //the + prefix casts it to an integer
+    //the slice method gets rid of "px"
+    let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+    //get CSS width
+    let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+    //scale the canvas
+    canvas.setAttribute('height', style_height * dpi);
+    canvas.setAttribute('width', style_width * dpi);
+}
+
 function $(selector) {
     return document.querySelector(selector);
 }
@@ -101,6 +115,7 @@ function update() {
 function canvas_update() {
     canvas.height = window.innerHeight-botttom_offset;
     canvas.width = window.innerWidth;
+    fix_dpi()
     update();
 }
 
